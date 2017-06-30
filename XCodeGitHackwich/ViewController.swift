e353be12584acd5ccd47200169625c8bb17ca532
Michael Giordano <michaelgiordano@Michaels-MacBook-Air.local>
2017-06-30 11:30:02 -0500

    anything

--- XCodeGitHackwich/ViewController.swift
+++ XCodeGitHackwich/ViewController.swift
@@ -12,10 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func onButtonTapped(_ sender: Any) {
-        print("Button Was Tapped")
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
+
 
 }
+
